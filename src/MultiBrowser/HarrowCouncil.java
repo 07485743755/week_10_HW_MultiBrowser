72d8b67099e4f30600c247d32598c1cace625c80
07485743755 <nidsdhola@gmail.com>
2021-05-09 22:35:18 +0100

    My week 10 HW multibrowser

---
+++ src/MultiBrowser/HarrowCouncil.java
@@ -0,0 +1,61 @@
+package MultiBrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class HarrowCouncil {
+
+
+
+    static String baseUrl = "https://www.harrow.gov.uk/";
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the Browser : ");
+        String browser = scan.nextLine();
+
+        if (browser.equalsIgnoreCase("chrome")) {
+
+            System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+            driver = new ChromeDriver();
+
+        } else if (browser.equalsIgnoreCase("firefox")) {
+
+            System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+            driver = new FirefoxDriver();
+
+        } else if (browser.equalsIgnoreCase("internet explorer")) {
+
+            System.setProperty("webdriver.ie.driver", "drivers/IEDriverServer.exe");
+            driver = new InternetExplorerDriver();
+
+        } else if (browser.equalsIgnoreCase("microsoft edge")) {
+
+            System.setProperty("webdriver.edge.driver", "drivers/msedgedriver.exe");
+            driver = new EdgeDriver();
+
+        } else {
+
+            System.out.println("Invalid Browser name");
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(baseUrl);
+
+        String title=driver.getTitle();
+        System.out.println("Title : " + title);
+        System.out.println("Length of title : "+ title.length());
+        System.out.println("Title contains 'homepage' : "+title.contains("homepage"));
+        driver.close();
+
+    }
+}
---
+++ src/MultiBrowser/X_Cart.java
@@ -0,0 +1,58 @@
+package MultiBrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class X_Cart {
+
+    static String baseUrl = "https://www.x-cart.com/";
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the Browser : ");
+        String browser = scan.nextLine();
+
+        if (browser.equalsIgnoreCase("chrome")) {
+
+            System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+            driver = new ChromeDriver();
+
+        } else if (browser.equalsIgnoreCase("firefox")) {
+
+            System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+            driver = new FirefoxDriver();
+
+        } else if (browser.equalsIgnoreCase("internet explorer")) {
+
+            System.setProperty("webdriver.ie.driver", "drivers/IEDriverServer.exe");
+            driver = new InternetExplorerDriver();
+
+        } else if (browser.equalsIgnoreCase("microsoft edge")) {
+
+            System.setProperty("webdriver.edge.driver", "drivers/msedgedriver.exe");
+            driver = new EdgeDriver();
+
+        } else {
+
+            System.out.println("Invalid Browser name");
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(baseUrl);
+
+        System.out.println("Title : " + driver.getTitle());
+
+        driver.close();
+
+    }
+
+}
